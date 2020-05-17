e96ecc4e7ba9f1ba9335b199b02f90582ea802bc
梦欤 <1501076885@qq.com>
2020-05-17 11:58:46 +0800

    ff

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,6 @@
 const constConf = require("../conf/constConf");
 const redis = require('redis');
-const client = redis.createClient(constConf.REDIS_PORT, constConf.REDIS_PASSWORD, {
+const client = redis.createClient(constConf.REDIS_PORT, constConf.REDIS_HOST, {
   password: constConf.REDIS_PASSWORD == '' ? undefined : constConf.REDIS_PASSWORD
 });
 
